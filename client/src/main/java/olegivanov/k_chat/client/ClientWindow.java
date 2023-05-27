6f2f44c8aae1246d572cafc08db0cc7524d20bb3
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-05-27 20:14:58 +0400

    Client window 1st stage - Dialog updated

--- client/src/main/java/olegivanov/k_chat/client/ClientWindow.java
+++ client/src/main/java/olegivanov/k_chat/client/ClientWindow.java
@@ -3,41 +3,69 @@ package olegivanov.k_chat.client;
 import javax.swing.*;
 import java.awt.*;
 
-public class ClientWindow extends JFrame  {
-    private static final String IP_ADDR = "127.0.0.1";
-    private static final int PORT = 8189;
+public class ClientWindow extends JFrame {
+    private static String ip_addr = "127.0.0.1";
+    private static  int port= 8189;
     private static final int WITH = 600;
     private static final int HEIGHT = 400;
     private final JTextArea msgFrame = new JTextArea();
     private final JLabel userLabel = new JLabel("User name: ");
-    private final JTextField nickName = new JTextField("Noname");
+    private static JTextField nickName = new JTextField();
     private final JLabel textLabel = new JLabel("Input your message: ");
     private final JTextField inputMessage = new JTextField();
 
     public static void main(String[] args) {
-SwingUtilities.invokeLater(new Runnable() {
-    @Override
-    public void run() {
-        new ClientWindow();
+        InputParameters();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
     }
-});
-    }
-    private ClientWindow(){
-setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Стандартный выход/закрытие по крестику из окна
-        setSize(WITH,HEIGHT); // задаем размеры размеры по-умодчанию
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Стандартный выход/закрытие по крестику из окна
+        setSize(WITH, HEIGHT); // задаем размеры размеры по-умодчанию
         setLocationByPlatform(true); // расположение задает Windows
         setAlwaysOnTop(true); // всегда  поверх экрана
-        setTitle("Simple Messenger"); // подписываем окно сообщений
+        setTitle("Simple Messenger - " + nickName.getText()); // подписываем окно сообщений
         setResizable(true); // может менять размер
         msgFrame.setLineWrap(true); // включение Wrap on
         msgFrame.setEditable(false); // не редактируемый
+        //add(userLabel,BorderLayout.NORTH);
+        //add(nickName,BorderLayout.AFTER_LINE_ENDS);
         add(msgFrame, BorderLayout.CENTER); // добавляем компоненты ..
-        add(userLabel, BorderLayout.NORTH);
-        add(nickName, BorderLayout.NORTH);
-        add(inputMessage, BorderLayout.SOUTH);
-
+        add(textLabel, BorderLayout.SOUTH);
+        add(inputMessage, BorderLayout.AFTER_LAST_LINE);
 
 
         setVisible(true); // делаем видимым
+    }
+
+    private static void InputParameters() {
+        JTextField addressInput = new JTextField("127.0.0.1",15);
+        JTextField portInput = new JTextField("8189",10);
+        JTextField nickInput = new JTextField("Noname",15);
+
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Server address: "));
+        myPanel.add(addressInput );
+        myPanel.add(Box.createHorizontalStrut(15)); //  spacer
+        myPanel.add(new JLabel("Port:"));
+        myPanel.add(portInput);
+        myPanel.add(Box.createHorizontalStrut(15)); //  spacer
+        myPanel.add(new JLabel("Nick Name:"));
+        myPanel.add(nickInput);
+
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+                "Please Enter Server address, port & your nick name", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION && addressInput.getText().equals("")
+                                            && portInput.getText().equals("")
+                                            && nickInput.getText().equals("")) {
+            ip_addr = addressInput.getText();
+            port = Integer.parseInt(portInput.getText());
+            nickName = nickInput;
+        }
     }
 }
