e79f1669f68b9b2f16a2673481853a699fc2e085
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-05-27 19:22:47 +0400

    Client window 1st stage

--- client/src/main/java/olegivanov/k_chat/client/ClientWindow.java
+++ client/src/main/java/olegivanov/k_chat/client/ClientWindow.java
@@ -1,7 +1,43 @@
 package olegivanov.k_chat.client;
 
-public class ClientWindow {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
 
+public class ClientWindow extends JFrame  {
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8189;
+    private static final int WITH = 600;
+    private static final int HEIGHT = 400;
+    private final JTextArea msgFrame = new JTextArea();
+    private final JLabel userLabel = new JLabel("User name: ");
+    private final JTextField nickName = new JTextField("Noname");
+    private final JLabel textLabel = new JLabel("Input your message: ");
+    private final JTextField inputMessage = new JTextField();
+
+    public static void main(String[] args) {
+SwingUtilities.invokeLater(new Runnable() {
+    @Override
+    public void run() {
+        new ClientWindow();
+    }
+});
+    }
+    private ClientWindow(){
+setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Стандартный выход/закрытие по крестику из окна
+        setSize(WITH,HEIGHT); // задаем размеры размеры по-умодчанию
+        setLocationByPlatform(true); // расположение задает Windows
+        setAlwaysOnTop(true); // всегда  поверх экрана
+        setTitle("Simple Messenger"); // подписываем окно сообщений
+        setResizable(true); // может менять размер
+        msgFrame.setLineWrap(true); // включение Wrap on
+        msgFrame.setEditable(false); // не редактируемый
+        add(msgFrame, BorderLayout.CENTER); // добавляем компоненты ..
+        add(userLabel, BorderLayout.NORTH);
+        add(nickName, BorderLayout.NORTH);
+        add(inputMessage, BorderLayout.SOUTH);
+
+
+
+        setVisible(true); // делаем видимым
     }
 }
